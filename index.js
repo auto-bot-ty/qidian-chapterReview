d2708be90c869a28fcadc4526b245d0794c40d87
auto-bot-ty <1093034894@qq.com>
2021-08-15 15:12:17 +0800

    chore: 优化

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const got = require("got");
 const books = require("./config.json").books;
 const { ProcessChapterReview, getNewList } = require("./utils");
-const csrfToken = "4H73EIrxxJaMWelkAN0MWZaU3SHGWkefJQQzlHWS";
 
 (async () => {
   for (const item of books) {
     if (!item.start) continue;
-    const newList = await getNewList(item.bookId, item.start, csrfToken);
-    for (const e of newList) {
-      if (!item.drift) e.cN = `[${e.uuid}] ${e.cN}`;
-      await ProcessChapterReview(item.bookId, e.id, e.cN, csrfToken);
+    const newList = await getNewList(item.bookId, item.start);
+    for (const chapter of newList) {
+      if (!item.drift) chapter.cN = `[${chapter.uuid}] ${chapter.cN}`;
+      await ProcessChapterReview(chapter.id, chapter.cN);
     }
   }
 })();
