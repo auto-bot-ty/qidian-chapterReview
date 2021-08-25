185963196659baa5de2ab8a5d7c6276a6ad44c5d
auto-bot-ty <1093034894@qq.com>
2021-08-25 10:15:57 +0800

    feat(utils): 获取全部章节

--- utils.js
+++ utils.js
@@ -10,18 +10,14 @@ const ProcessChapterReview = async (chapterId, chapterName) => {
     reviewSummary.map(async (item) => {
       const pageSize = item.reviewNum > 98 ? 98 : item.reviewNum;
       const chapterReviewUrl = `https://vipreader.qidian.com/ajax/chapterReview/reviewList?_csrfToken=${csrfToken}&bookId=${bookId}&chapterId=${chapterId}&segmentId=${item.segmentId}&type=2&page=1&pageSize=${pageSize}`;
-      const response = await got.get(chapterReviewUrl);
+      const response = await got(chapterReviewUrl);
       try {
         const list = JSON.parse(response.body).data.list;
-        if (list.length > 0) {
-          const content = list.map(
-            (item) => `>--- ${item.content.trim()}<br>\n`
-          );
-          const quoteContent = [
-            `\n[${item.segmentId}] ${list[0].quoteContent.trim()}\n`,
-          ];
-          return [...quoteContent, ...content];
-        }
+        const content = list.map((item) => `>--- ${item.content.trim()}<br>\n`);
+        const quoteContent = [
+          `\n[${item.segmentId}] ${list[0].quoteContent.trim()}\n`,
+        ];
+        return [...quoteContent, ...content];
       } catch (err) {
         const msg = `[error] invalid list (${item.segmentId})\n---response: ${response.body}\n---chapterReviewUrl: ${chapterReviewUrl}`;
         console.log(msg);
@@ -37,12 +33,12 @@ const getNewList = async (bid, start) => {
   mkdirBookDir();
   csrfToken = await getCsrfToken();
   const categoryUrl = `https://m.qidian.com/majax/book/category?_csrfToken=${csrfToken}&bookId=${bookId}`;
-  const response = await got.get(categoryUrl);
+  const response = await got(categoryUrl);
   const data = JSON.parse(response.body).data;
-  const vs = data.vs;
   const bookName = data.bookName;
-  console.log(`${bookName}\n================================`);
-  const list = vs[vs.length - 1].cs;
+  console.log(`\n${bookName}\n================================`);
+  let list = [];
+  data.vs.forEach((e) => (list = [...list, ...e.cs]));
   start = start > list.length ? list.length : start;
   console.log(`抓取最新 ${start} 章`);
   const newList = list.slice(-start);
@@ -51,14 +47,14 @@ const getNewList = async (bid, start) => {
 
 const getReviewSummary = async (chapterId) => {
   const reviewSummaryUrl = `https://read.qidian.com/ajax/chapterReview/reviewSummary?_csrfToken=${csrfToken}&bookId=${bookId}&chapterId=${chapterId}`;
-  const response = await got.get(reviewSummaryUrl);
+  const response = await got(reviewSummaryUrl);
   const reviewSummary = JSON.parse(response.body).data.list;
   reviewSummary.sort((a, b) => a.segmentId - b.segmentId);
-  return reviewSummary;
+  return reviewSummary.filter((e) => (e.reviewNum = 0));
 };
 
 const getCsrfToken = async () => {
-  const response = await got.get(`https://m.qidian.com/book/${bookId}/catalog`);
+  const response = await got(`https://m.qidian.com/book/${bookId}/catalog`);
   const _csrfToken = response.headers["set-cookie"]
     .join("")
     .match(/_csrfToken=(\S*);/)[1];
