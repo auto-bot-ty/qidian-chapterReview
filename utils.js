b25e25d7079a2f7090726925cfaeba616fa0d744
auto-bot-ty <1093034894@qq.com>
2022-02-20 11:02:52 +0000

    add utils/got

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const got = require("got");
+const got = require("./utils/got");
 const { logger, errorLogger } = require("./logs/logger");
 let path;
 let bookId;
@@ -13,7 +13,7 @@ const ProcessChapterReview = async (chapterId, chapterName) => {
       const pageSize = item.reviewNum;
       const chapterReviewUrl = `https://vipreader.qidian.com/ajax/chapterReview/reviewList?_csrfToken=${csrfToken}&bookId=${bookId}&chapterId=${chapterId}&segmentId=${item.segmentId}&type=2&page=1&pageSize=${item.pageSize}`;
       const response = await got(chapterReviewUrl);
-      const { list } = JSON.parse(response.body).data;
+      const { list } = response.data.data;
       try {
         const content = list.map((item) => `>--- ${item.content.trim()}<br>\n`);
         const quoteContent = [
@@ -34,7 +34,7 @@ const ProcessChapterReview = async (chapterId, chapterName) => {
 const getNewList = async (bid, start) => {
   await assignmentGlobalVariables(bid);
   const categoryUrl = `https://m.qidian.com/majax/book/category?_csrfToken=${csrfToken}&bookId=${bookId}`;
-  const data = await got(categoryUrl).then((res) => JSON.parse(res.body).data);
+  const { data } = await got(categoryUrl).then((res) => res.data);
   const { bookName } = data;
   logger.info(
     `${bookName}  (${
@@ -57,7 +57,7 @@ const getNewList = async (bid, start) => {
 const getReviewSummary = async (chapterId) => {
   const reviewSummaryUrl = `https://read.qidian.com/ajax/chapterReview/reviewSummary?_csrfToken=${csrfToken}&bookId=${bookId}&chapterId=${chapterId}`;
   const reviewSummary = await got(reviewSummaryUrl).then(
-    (res) => JSON.parse(res.body).data.list
+    (res) => res.data.data.list
   );
   reviewSummary.sort((a, b) => a.segmentId - b.segmentId);
   return reviewSummary.filter((e) => e.reviewNum !== 0);
@@ -70,11 +70,9 @@ const assignmentGlobalVariables = async (bid) => {
 };
 
 const fetchCsrfToken = async () => {
-  const response = await got(`https://m.qidian.com/book/${bookId}/catalog`);
-  const _csrfToken = response.headers["set-cookie"]
-    .join("")
-    .match(/_csrfToken=(\S*);/)[1];
-  return _csrfToken;
+  return await got(`https://m.qidian.com/book/${bookId}/catalog`).then(
+    (res) => res.headers["set-cookie"].join("").match(/_csrfToken=(\S*);/)[1]
+  );
 };
 
 const createBookDir = () => {
@@ -93,9 +91,7 @@ const writeFile = (out, chapterName) => {
     out.join("").replace(/,/g, ""),
     { flag: "w" },
     (err) => {
-      logger.info(
-        err ? `${chapterName} 写入失败！` : `${chapterName} 写入成功！`
-      );
+      err ? errorLogger.info(`${chapterName} 写入失败！\n ${err}`) : logger.info(`${chapterName} 写入成功！`)
     }
   );
 };
---
+++ utils/got.js
@@ -0,0 +1,39 @@
+const { errorLogger } = require("../logs/logger");
+const got = require('got');
+
+const custom = got.extend({
+    retry: 2,
+    hooks: {
+        beforeRetry: [
+            (options, err, count) => {
+                errorLogger.info(`Request ${options.url} fail, retry attempt #${count}: ${err}`);
+            },
+        ],
+        afterResponse: [
+            (response) => {
+                try {
+                    response.data = JSON.parse(response.body);
+                } catch (e) {
+                    response.data = response.body;
+                }
+                response.status = response.statusCode;
+                return response;
+            },
+        ],
+        init: [
+            (options) => {
+                // compatible with axios api
+                if (options && options.data) {
+                    options.body = options.body || options.data;
+                }
+            },
+        ],
+    },
+    headers: {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    },
+    timeout: 30000,
+});
+custom.all = (list) => Promise.all(list);
+
+module.exports = custom;
