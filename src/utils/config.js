d2d464c4c34c9d18a336f87143ffb09ae83040e4
auto-bot-ty <1093034894@qq.com>
2022-05-02 02:07:58 +0000

    feat(config): 添加 `OUTPUT_PATH` 配置

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,10 @@
-const value = {
+require('dotenv').config();
+let envs = process.env;
+let value = {
   ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
   requestRetry: 2,
   requestTimeout: 30000,
-  prefixPath: "../output/",
+  prefixPath: envs.OUTPUT_PATH || "../output/",
   githubRepository: "auto-bot-ty/qidian-chapterReview",
 };
 
